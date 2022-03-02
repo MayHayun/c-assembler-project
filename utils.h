06f6543ebab4e7572729c2dd4636150d76d89ad0
Avichai Greenblum <66025573+Avichai11Greenblum@users.noreply.github.com>
2022-03-02 17:32:27 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -6,7 +6,23 @@ typedef struct string
     char *data;
 }string;
 
+typedef struct commandsStruct{
+    char command[30];
+    char ARE;
+    int opcode;
+    int funct;
+    int numOfParam;
+    *char sourceAble[10];
+    *char destAble[10];
+
+
+}commandStruct;
+
+typedef struct commandData{
+    char name[10];
+    int valid;
+    int bytes;
+
+}commandData;
+
 int move_to_none_white(char line[], int i);
-
-
-
