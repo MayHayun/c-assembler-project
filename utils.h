2dd90915431c957ce8c09c53c04629654dd21895
Avichai Greenblum <66025573+Avichai11Greenblum@users.noreply.github.com>
2022-03-02 20:44:38 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -12,8 +12,8 @@ typedef struct commandsStruct{
     int opcode;
     int funct;
     int numOfParam;
-    **char sourceAble; /* legit source functions */
-    **char destAble;   /* legit destination functions */
+    char** sourceAble; /* legit source functions */
+    char** destAble;   /* legit destination functions */
 
 
 }commandStruct;
