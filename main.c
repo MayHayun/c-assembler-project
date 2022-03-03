8908a978f956f53ebd303bdc31f6dd20bd81f7ec
Avichai Greenblum <66025573+Avichai11Greenblum@users.noreply.github.com>
2022-03-03 12:36:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 #include <string.h>
+#define FILE_NAME_MAX_SIZE 40
+
+/* func to check if the ending of the file is valid, returns 1 if it's valid */
+int fileEndingValid( char *fileName ){
+
+    int i;
+    for( i=0; fileName[i] != 0; i++ ){
+        
+        if( i != 0 && fileName[i] == '.' ){
+
+            if( strcmp( (fileName+i), ".as") == 0 )
+                return 1;
+        }
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]){
 
+    int i, j;
+    int duplicateExists = 0;
+    char fileName[FILE_NAME_MAX_SIZE];
+
     if ( argc == 1 ){
         printf("No files were given for the program...");
+        exit(0);
+    }
+    
+    for( i=1; i < argc; i++){
+
+        strcpy(fileName, argv[i]);
+        for( j = 1; j < i; j++ ){
+
+            if( !strcmp(fileName, argv[j]) )
+                duplicateExists = 1;
+        }
+
+        if( !duplicateExists ){
+            if( fileEndingValid(fileName) ){
+                
+                DO THE BIG BAD THING OF THE WHOLE THING
+            }
+            else{
+                printf("The ending of the file needs to be \".as\"  ...");
+            }
+        }
+
     }
 }
