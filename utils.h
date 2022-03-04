7546bcdefee535bcd5218bfd31efa32ca0a3969f
Avichai Greenblum <66025573+Avichai11Greenblum@users.noreply.github.com>
2022-03-04 15:57:26 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct string
-{
+#define COMMAND_MAX_LENGTH 31
+
+
+typedef struct string{
     char *data;
 }string;
 
 typedef struct commandsStruct{
-    char command[30];
-    char ARE;
+    char commandName[COMMAND_MAX_LENGTH];
     int opcode;
     int funct;
     int numOfParam;
-    char** sourceAble; /* legit source functions */
-    char** destAble;   /* legit destination functions */
-
-
-}commandStruct;
+    /*char** sourceAble;  legit source functions */
+    /*char** destAble;    legit destination functions */
+     
+}commandsStruct;
 
 typedef struct commandData{
     char name[10];
     int valid;
     int bytes;
-
 }commandData;
 
 int move_to_none_white(char line[], int i);
